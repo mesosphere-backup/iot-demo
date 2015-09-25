2d10de0ff083e847c1a3464ade3975e1d989cdbb
Peter Kolloch <peter@mesosphere.io>
2015-09-25 18:31:33 +0200

    Create yaml processor, scripts, documentation

--- twitter/src/main/scala/core/main.scala
+++ twitter/src/main/scala/core/main.scala
@@ -6,11 +6,18 @@ import org.apache.log4j.BasicConfigurator
 object Main extends App {
   BasicConfigurator.configure()
 
+  println("args: " + args)
+
   val query = args.mkString(" ")
+  if (query.trim.isEmpty) {
+    println("No query was given!!")
+    sys.exit(1)
+  }
+
   println(s"Running with query: $query")
 
   val system = ActorSystem()
-  val producer = system.actorOf(Props(new ProducerActor))
+  val producer = system.actorOf(Props(new ProducerActor(query)))
   val stream = system.actorOf(Props(
     new TweetStreamerActor(TweetStreamerActor.twitterUri, producer, query)))
 
